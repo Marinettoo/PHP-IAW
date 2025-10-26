f5d967f4dd9bff3ebadbf349d684e72724b9367b
marinettoo <jesusperezmarinetto@hotmail.com>
2025-10-26 20:56:15 +0100

    forma dificil pag 109

---
+++ 05.Strings/02.Pag109 (Forma Chunga).php
@@ -0,0 +1,15 @@
+<?php
+$palabra = "oro";
+
+$invertida = "";
+
+for ($i = strlen($palabra); $i >= 0; $i--){
+    $invertida .= $palabra[$i];
+}
+
+if ($palabra == $invertida){
+    echo 'es un palíndromo';
+}else{
+    echo 'no es un palíndromo';
+}
+?>
