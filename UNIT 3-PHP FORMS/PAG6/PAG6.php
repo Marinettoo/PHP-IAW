5ee0d2c253a3ea7b5f57a3dfbb37798d49fd748f
marinettoo <jesusperezmarinetto@hotmail.com>
2025-11-04 12:45:39 +0100

    Exercise pag 10

--- UNIT 3-PHP FORMS/PAG6/PAG6.php
+++ UNIT 3-PHP FORMS/PAG6/PAG6.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-#Write an HTML form to ask for a person’s name and mail and send them to this script. Then add the var_dump function to read the content of the $_POST variable.
     
 <html lang=”en”>
    <head>
@@ -7,6 +6,7 @@
    </head>
    <body>
       <?php
+      #Write an HTML form to ask for a person’s name and mail and send them to this script. Then add the var_dump function to read the content of the $_POST variable.
        echo "<p>Welcome, ", $_POST["name"], "(", $_POST["mail"], ").</p>";
          var_dump($_POST);
        ?>
